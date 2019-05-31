ab11316bccbbbe6feeeb67291b306dda62dbad1c
Mart van de Sanden <m.vandesanden@kopano.com>
2019-05-31 16:58:00 +0200

    fix to init.sh

--- src/scripts/config.sh
+++ src/scripts/config.sh
@@ -4,4 +4,6 @@ ROOT_NS="MvdS"
 
 MVDS_PROJECT_BASE_DIRECTORY=$(git rev-parse --show-toplevel)
 
+echo "Project base directory: $MVDS_PROJECT_BASE_DIRECTORY"
+
 CLANG_FORMAT=clang-format-7
--- src/scripts/init.sh
+++ src/scripts/init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-BASE="$(dirname ${BASH_SOURCE[0]})"
+BASE="$(dirname $0)"
 source $BASE/config.sh
 
 export PATH=$MVDS_PROJECT_BASE_DIRECTORY/src/scripts/:$PATH
