08710caa6da1bbb5ff597d2e6ef23a2a9c06c77e
Mart van de Sanden <m.vandesanden@kopano.com>
2019-06-04 09:19:09 +0200

    work in progress

--- src/groups/ksv/ksvc/ksvc_commandlineargs.cpp
+++ src/groups/ksv/ksvc/ksvc_commandlineargs.cpp
@@ -17,7 +17,7 @@ namespace {
 
 #define _(x, y, z) (CommandlineArgs::Data *x, const std::string &y, const std::string &z)
   
-  struct {
+  struct ArgInfo {
     // DATA
     const std::string d_longName;
     const std::string d_shortName;
@@ -25,11 +25,43 @@ namespace {
     std::function<void(CommandlineArgs::Data *data, const std::string &name,
                        const std::string &value)>
         d_cb;
-  } ARGS[] = {{"config", "c", "", [] _(data, name, value) {
-                 data->d_configFiles.emplace_back(value);
-               }}};
+  };
 
+  ArgInfo s_argList[] = {{"config", "c", "", [] _(data, name, value) {
+                       data->d_configFiles.emplace_back(value);
+                     }}};
+
+  auto s_argList[] = std::make_{{"config", "c", "", [] _(data, name, value) {
+                       data->d_configFiles.emplace_back(value);
+                     }}};
+
+  
 #undef _
+
+  const ArgInfo *findArg(const std::string &name,
+                         const std::function < bool(const std::string &name,
+                                                    const ArgInfo &arg) &
+                             cmp) {
+    const auto end = s_argList + sizeof(s_argList) / sizeof(s_argList[0]);
+    auto argIter = std::find_if(s_argList, end, cmp);
+
+    if (end == argIter) {
+      return;
+    }
+  }
+
+  const ArgInfo *findArgLong(const std::string& name)
+  {
+    return findArg(name,
+                   [name](const auto &arg) { return name == arg.d_longName; });
+  }
+
+  const ArgInfo *findArgShort(const std::string& name)
+  {
+    return findArg(name,
+                   [name](const auto &arg) { return name == arg.d_shortName; });
+  }
+
   
 } // anonymouse namespace
 
@@ -49,10 +81,7 @@ CommandlineArgs::~CommandlineArgs() {}
 void CommandlineArgs::addLong(const std::string &name,
                               const std::string &value)
 {
-  const auto end = ARGS + sizeof(ARGS) / sizeof(ARGS[0]);
-  auto argIter = std::find_if(ARGS, end, [name](const auto &arg) {
-    return name == arg.d_longName;
-  });
+  ArgInfo *arg = findArgLong(name);
 
   if (end == argIter) {
     return;
