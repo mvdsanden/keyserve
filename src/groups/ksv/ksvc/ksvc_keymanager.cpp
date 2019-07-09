a2cc0e5ac3d1a0e902a9cdb6d0f32fe60381fb1b
Mart van de Sanden <m.vandesanden@kopano.com>
2019-07-09 15:08:05 +0200

    update to key management

--- src/groups/ksv/ksvc/ksvc_keymanager.cpp
+++ src/groups/ksv/ksvc/ksvc_keymanager.cpp
@@ -16,21 +16,37 @@ class KeyManagerImpl : public KeyManager
   Crypto *  d_crypto;
 
   // PRIVATE MANIPULATORS
-  
+  void createCryptoKey(ResultFunction<std::shared_ptr<CryptoKey>> result,
+                       std::string                                parent,
+                       std::string                                cryptoKeyId,
+                       CryptoKey                                  cryptoKey,
+                       const ResultStatus &                       versionStatus,
+                       std::shared_ptr<CryptoKeyVersion>          version)
+  {
+    if (!versionStatus.isSuccess()) {
+      result(versionStatus, nullptr);
+      return;
+    }
+    
+    d_keyStore->createCryptoKey(std::move(result),
+                                std::move(parent),
+                                std::move(cryptoKeyId),
+                                std::move(cryptoKey),
+                                std::move(version));
+  }
+
 public:
   // CREATORS
   KeyManageImpl(KeyStore *keyStore, Crypto *crypto, const Configuration &config)
-    : d_keyStore(keyStore)
-    , d_crypto(crypto)
-  {
-  }
-  
+      : d_keyStore(keyStore)
+      , d_crypto(crypto)
+  {}
+
   // MANIPULATORS
-  void
-  KeyManager::createKeyRing(ResultFunction<std::shared_ptr<KeyRing>> result,
-                            std::string                              parent,
-                            std::string                              keyRingId,
-                            KeyRing                                  keyRing)
+  void createKeyRing(ResultFunction<std::shared_ptr<KeyRing>> result,
+                     std::string                              parent,
+                     std::string                              keyRingId,
+                     KeyRing                                  keyRing)
   {
     d_keyStore->createKeyRing(std::move(result),
                               std::move(parent),
@@ -38,24 +54,32 @@ public:
                               std::move(keyRing));
   }
 
-  void
-  KeyManager::createCryptoKey(ResultFunction<std::shared_ptr<CryptoKey>> result,
-                              std::string                                parent,
-                              std::string cryptoKeyId,
-                              CryptoKey   cryptoKey,
-                              bool        skipInitialVersionCreation = false)
+  void createCryptoKey(ResultFunction<std::shared_ptr<CryptoKey>> result,
+                       std::string                                parent,
+                       std::string                                cryptoKeyId,
+                       CryptoKey                                  cryptoKey,
+                       bool skipInitialVersionCreation = false)
   {
-    std::shared_ptr<CryptoKeyVersion> version;
-    
-    if (!skipInitialVersionCreation) {
-      version = d_crypto->createCryptoKey(cryptoKey);
-    }    
+    if (skipInitialVersionCreation) {
+      createCryptoKey(std::move(result),
+                      std::move(parent),
+                      std::move(cryptoKeyId),
+                      std::move(cryptoKey),
+                      ResultStatus::e_success,
+                      nullptr);
+      return;
+    }
 
-    d_keyStore->createCryptoKey(std::move(result),
-				std::move(parent),
-				std::move(cryptoKeyId),
-				std::move(cryptoKey),
-				std::move(version));
+    using namespace std::placeholders;
+    d_crypto->createCryptoKey(std::bind(&KeyManager::createCryptoKey,
+                                        this,
+                                        std::move(result),
+                                        std::move(parent),
+                                        std::move(cryptoKeyId),
+                                        std::move(cryptoKey),
+                                        _1,
+                                        _2),
+                              cryptoKey);
   }
 
   // ACCESSORS
